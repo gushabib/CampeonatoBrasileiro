54552b9ac3a840c5868cfaabaaff83f500c5b463
Gustavo Habib <gushabib@gmail.com>
2024-10-15 09:21:18 -0300

    Resolvendo o primeiro problema: O time que mais venceu jogos no ano 2008

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,48 @@
-import Dados.Campeonato;
+import Dados.Dados;
 
-import java.util.List;
-
-import static Dados.Dados.*;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        // List<Cartoes> cartoes = cartoes();
-        // List<Gols> gols = gols();
-        //List<Estatisticas> estatisticas = estatisticas();
-        List<Campeonato> campeonato = campeonato();
 
-        if (!campeonato.isEmpty()) {
-            System.out.println("Cartões lidos do CSV:");
-            for (Campeonato campeonatos : campeonato) {
-                System.out.println(campeonatos);
-            }
-        } else {
-            System.out.println("Nenhum gol foi encontrado ou houve um erro ao ler o arquivo.");
-        }
+//        O time que mais venceu jogos no ano 2008
+        var maiorNumerosDePartidasVencidas2008 =  Dados.campeonato().stream()
+                .filter(data -> data.getData().contains("2008"))
+                .filter(campo -> !Objects.equals(campo.getVencedor(), "-"))
+                .collect(Collectors.groupingBy(
+                        vencedor -> vencedor.getVencedor(),
+                        Collectors.counting())).entrySet().stream()
+                .max(Comparator.comparing(Map.Entry::getValue))
+                .map(Map.Entry::getValue).orElse(null);
+
+        var timeQueMaisVenceu2008 = Dados.campeonato().stream()
+                .filter(data -> data.getData().contains("2008"))
+                .filter(campo -> !Objects.equals(campo.getVencedor(), "-"))
+                .collect(Collectors.groupingBy(
+                        vencedor -> vencedor.getVencedor(),
+                        Collectors.counting())).entrySet().stream().filter(valor -> {
+                    return valor.getValue().equals(maiorNumerosDePartidasVencidas2008);
+                }).collect(Collectors.toList());
+
+
+
+
+
+
+        //System.out.println(timeQueMaisVenceu2008.toString().split("=")[1].split("]")[0]);
+        System.out.println(timeQueMaisVenceu2008);
+//        O Estado que teve menos jogos dentro do período 2003 e 2022
+//        O jogador que mais fez gols
+//        O jogador que mais fez gols de pênaltis
+//        O jogador que mais fez gols contras
+//        O jogador que mais recebeu cartões amarelos
+//        O jogador que mais recebeu cartões vermelhos
+//        O placar da partida com mais gols.
+
+
+
     }
 }
